52cec80b9eb6b249e929d64612803207cd935e2e
LuoJingYan <921402781@qq.com>
2020-07-27 00:10:04 +0800

    搭建项目结构

---
+++ BusinessSystem/src/main/java/com/bolo/auction/BussinessSystemStarter.java
@@ -0,0 +1,23 @@
+package com.bolo.auction;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@SpringBootApplication
+@RestController
+public class BussinessSystemStarter {
+
+    public static void main(String[] args) {
+        log.info("业务系统启动中...");
+        SpringApplication.run(BussinessSystemStarter.class, args);
+    }
+
+    @RequestMapping("hello")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ WebSocketService/src/main/java/com/bolo/auction/WebSocketServiceStarter.java
@@ -0,0 +1,14 @@
+package com.bolo.auction;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class WebSocketServiceStarter {
+    public static void main(String[] args) {
+        log.info("WebSocket服务端启动中...");
+        SpringApplication.run(WebSocketServiceStarter.class, args);
+    }
+}
